d5ee430d8967e43da0266f7206619390f9ad06b0
vsdev <diversantvlz@gmail.com>
2018-09-06 21:56:32 +0300

    initial

---
+++ src/Integration/DataProvider.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 06.09.2018 20:40 Vacheslav Silyutin <diversantvlz@gmail.com>
+ */
+
+namespace src\Integration;
+
+class DataProvider
+{
+    private $host;
+    private $user;
+    private $password;
+
+    /**
+     * @param $host
+     * @param $user
+     * @param $password
+     */
+    public function __construct($host, $user, $password)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    /**
+     * @param array $request
+     *
+     * @return array
+     */
+    public function get(array $request)
+    {
+        // returns a response from external service
+    }
+}
